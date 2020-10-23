c21ab0291fc27cff36899416a817eb217ca953cd
romuch <neverkiller2@gmail.com>
2020-10-23 20:45:58 +0300

    Feat: farming contract + factory + some tests

--- contracts/DuckToken.sol
+++ contracts/DuckToken.sol
@@ -7,7 +7,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 //@todo check name and symbol
 contract DuckToken is ERC20("DuckToken", "DLC"), Ownable {
 
+	uint public constant PRESALE_SUPPLY 	= 20000000*10**18;
+	uint public constant TEAM_SUPPLY 			= 10000000*10**18;
+	uint public constant MAX_FARMING_POOL = 70000000*10**18;
+
+	mapping(address => bool) liquidityPools;
+	uint public currentFarmingPool;
+
+	constructor(address presaleWallet, address teamWallet) public {
+		_mint(presaleWallet, PRESALE_SUPPLY);
+		_mint(teamWallet, TEAM_SUPPLY);
+	}
+	
+	function addLiquidityPool(address liquidityPool) public onlyOwner {
+		liquidityPools[liquidityPool] = true;
+	}
+
 	function mint(address _to, uint256 _amount) public onlyOwner {
+		require(currentFarmingPool.add(_amount) <= MAX_FARMING_POOL, "exceed farming amount");
+		currentFarmingPool += _amount; 
     _mint(_to, _amount);
   }
+
+  function transfer(address recipient, uint256 amount) public override returns (bool) {
+  	if(liquidityPools[msg.sender]) {
+  	  _burn(msg.sender, amount);
+  	  return true;
+  	}
+
+  	return super.transfer(recipient, amount);
+  } 
 }
--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -5,10 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./DuckToken.sol";
+import "./PoolController.sol";
 
 contract Pool {
 
-	using SafeMath for uint256;
+  using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
  	// Info of each user.
@@ -22,35 +23,62 @@ contract Pool {
 		uint blocks;
 		uint farmingSupply;
 		uint tokensPerBlock;
-		bool isLockUp;
 	}
 
 	Period[] public periods;
-	address public factory;
+	PoolController public controller;
 	uint public lastRewardBlock;
-	IERC20 public lpToken;
+	
+
+  DuckToken public duck;
+  IERC20 public lpToken;
+  uint public lpSupply;
+
+  uint public rewardPerShare;
+  uint public accDuckPerShare;
+
+  mapping(address => UserInfo) public userInfo;
+
+  event Deposit(address indexed from, uint amount);
+  event Withdraw(address indexed to, uint amount);
+  event NewPeriod(uint indexed startingBlock, uint indexed blocks, uint farmingSupply);
 
 	modifier onlyFactory() { 
-		require(msg.sender == factory); 
+		require(msg.sender == address(controller)); 
 		_; 
 	}
 	
-	constructor(address _lpToken) public {
-		factory = msg.sender;
+	constructor(address _lpToken, uint _startingBlock, uint[] memory _blocks, uint[] memory _farmingSupplies) public {
+    require(_blocks.length > 0, "emply data");
+    require(_blocks.length == _farmingSupplies.length, "invalid data");
+    
+		controller = PoolController(msg.sender);
 		lpToken = IERC20(_lpToken);
+
+    addPeriod(_startingBlock, _blocks[0], _farmingSupplies[0]);
+    uint _bufStartingBlock = _startingBlock.add(_blocks[0]).add(1);
+
+    for(uint i = 1; i < _blocks.length; i++) {
+      addPeriod(_bufStartingBlock, _blocks[i], _farmingSupplies[i]);
+      _bufStartingBlock = _bufStartingBlock.add(_blocks[i]).add(1);
+    }
 	}
 	
-	function addPeriod(uint startingBlock, uint blocks, uint farmingSupply, bool isLockUp) public {
+	function addPeriod(uint startingBlock, uint blocks, uint farmingSupply) public onlyFactory {
+    if(periods.length > 0) {
+      require(startingBlock > periods[periods.length-1].startingBlock.add(periods[periods.length-1].blocks), "two periods in the same time");
+    }
+
 		uint tokensPerBlock = farmingSupply.div(blocks);
 		Period memory newPeriod = Period({
 			startingBlock: startingBlock,
 			blocks: blocks,
 			farmingSupply: farmingSupply,
-			tokensPerBlock: tokensPerBlock,
-			isLockUp: isLockUp
+			tokensPerBlock: tokensPerBlock
 		});
 
 		periods.push(newPeriod);
+    emit NewPeriod(startingBlock, blocks, farmingSupply);
 	}
 
 	// Update reward variables of the given pool to be up-to-date.
@@ -59,40 +87,63 @@ contract Pool {
         return;
     }
 
-    uint256 lpSupply = lpToken.balanceOf(address(this));
     if (lpSupply == 0) {
       lastRewardBlock = block.number;
       return;
     }
 
-    // uint256 reward = tokensPerBlock.mul(pool.allocPoint).div(totalAllocPoint);
-    // factory.mint(devaddr, sushiReward.div(10));
-    // factory.mint(address(this), sushiReward);
-    // pool.accSushiPerShare = pool.accSushiPerShare.add(sushiReward.mul(1e12).div(lpSupply));
-    // pool.lastRewardBlock = block.number;
+    uint256 duckReward = calculateDuckTokensForMint();
+    
+    controller.mint(controller.devAddress(), duckReward.div(100).mul(7));
+    controller.mint(address(this), duckReward.div(100).mul(93));
+
+    accDuckPerShare = accDuckPerShare.add(duckReward.mul(1e12).div(lpSupply));
+    lastRewardBlock = block.number;
   }
 
-//   function deposit(uint256 _pid, uint256 _amount) public {
-//     PoolInfo storage pool = poolInfo[_pid];
-//     UserInfo storage user = userInfo[_pid][msg.sender];
+  function deposit(uint256 _amount) public {
+    UserInfo storage user = userInfo[msg.sender];
+
+    updatePool();
     
-//     updatePool();
-//     if (user.amount > 0) {
-//         uint256 pending = user.amount.mul(pool.accSushiPerShare).div(1e12).sub(user.rewardDebt);
-//         if(pending > 0) {
-//             safeSushiTransfer(msg.sender, pending);
-//         }
-//     }
-// 		if(_amount > 0) {
-//         pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
-//         user.amount = user.amount.add(_amount);
-//     }
-//     user.rewardDebt = user.amount.mul(pool.accSushiPerShare).div(1e12);
-//     emit Deposit(msg.sender, _pid, _amount);
-// 	}
+    if (user.amount > 0) {
+      uint256 pending = user.amount.mul(accDuckPerShare).div(1e12).sub(user.rewardDebt);
+      if(pending > 0) {
+        safeDuckTransfer(msg.sender, pending);
+      }
+    }
+    
+    if(_amount > 0) {
+      lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+      user.amount = user.amount.add(_amount);
+      lpSupply = lpSupply.add(_amount);
+    }
+    
+    user.rewardDebt = user.amount.mul(accDuckPerShare).div(1e12);
+    
+    emit Deposit(msg.sender, _amount);
+  }
 
-	
-
+  function withdraw(uint256 _amount) public {
+    UserInfo storage user = userInfo[msg.sender];
+    
+    require(user.amount >= _amount, "withdraw: not good");
+    updatePool();
+    
+    uint256 pending = user.amount.mul(accDuckPerShare).div(1e12).sub(user.rewardDebt);
+    
+    if(pending > 0) {
+      safeDuckTransfer(msg.sender, pending);
+    }
+    
+    if(_amount > 0) {
+      user.amount = user.amount.sub(_amount);
+      lpToken.safeTransfer(address(msg.sender), _amount);
+    }
+    
+    user.rewardDebt = user.amount.mul(accDuckPerShare).div(1e12);
+    emit Withdraw(msg.sender, _amount);
+  }
 
   function getCurrentPeriodIndex() public view returns(uint) {
   	for(uint i = 0; i < periods.length; i++) {
@@ -102,7 +153,7 @@ contract Pool {
   	}
   }
 
-  function getTotalTokensPerBlock() public view returns(uint) {
+  function calculateDuckTokensForMint() public view returns(uint) {
   	uint totalTokens;
   	bool overflown;
 
@@ -126,7 +177,17 @@ contract Pool {
 
   			break;
   		}
-
   	}
   }
+
+  // Safe duck transfer function, just in case if rounding error causes pool to not have enough DUCKs.
+  function safeDuckTransfer(address _to, uint256 _amount) internal {
+    uint256 duckBal = duck.balanceOf(address(this));
+    if (_amount > duckBal) {
+      duck.transfer(_to, duckBal);
+    } else {
+      duck.transfer(_to, _amount);
+    }
+  }
+
 }
